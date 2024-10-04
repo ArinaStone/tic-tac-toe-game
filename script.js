a0f685216ef802bc23340bb0f71a7bb401310120
ArinaStone <Arina.Podkamennaya19@gmail.com>
2024-10-04 17:30:51 +0300

    Style updated

--- script.js
+++ script.js
@@ -1,66 +1,68 @@
 'use strict';
 
+// Начальные настройки
 let currentPlayer = 'X';
 let gameBoard = ['', '', '', '', '', '', '', '', ''];
 let gameActive = true;
+
 const message = document.getElementById('gameMessage');
 message.classList.remove('message-appear');
 
+// Смена игрока
 function handlePlayerTurn(clickedCellIndex) {
+    // Проверка клетки и активна ли игра
     if (gameBoard[clickedCellIndex] !== '' || !gameActive) {
         return;
     }
+    // Установка символа текущего игрока и переключение хода
     gameBoard[clickedCellIndex] = currentPlayer;
     checkForWinOrDraw();
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
 }
 
+// Выбор всех ячеек и добавление слушателя событий
+const cells = document.querySelectorAll('.gameboard__cell');
+cells.forEach(cell => {
+    cell.addEventListener('click', cellClicked, false);
+});
+
+// Обработка кликов по ячейкам
 function cellClicked(clickedCellEvent) {
+    // Проверка идентификатора ячейки
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = parseInt(clickedCell.id.replace('cell-', '')) - 1;
     if (gameBoard[clickedCellIndex] !== '' || !gameActive) {
         return;
     }
+    // Переключение хода и обновление интерфейса
     handlePlayerTurn(clickedCellIndex);
     updateUI();
+    clickedCell.style.backgroundColor = '#fae798';
 }
 
-const cells = document.querySelectorAll('.cell');
-
-cells.forEach(cell => {
-    cell.addEventListener('click', cellClicked, false);
-});
-
+// Обновление интерфейса
 function updateUI() {
     for (let i = 0; i < cells.length; i++) {
         cells[i].innerText = gameBoard[i];
     }
 }
 
-function announceWinner(player) {
-    message.innerText = `🎉 Player ${player} Wins!`;
-    message.classList.add('message-appear');
-}
-
-function announceDraw() {
-    message.innerText = 'Game Draw!';
-    message.classList.add('message-appear');
-}
-
+// Возможные выигрышные комбинации
 const winConditions = [
-  [0, 1, 2], // Top row
-  [3, 4, 5], // Middle row
-  [6, 7, 8], // Bottom row
-  [0, 3, 6], // Left column
-  [1, 4, 7], // Middle column
-  [2, 5, 8], // Right column
-  [0, 4, 8], // Left-to-right diagonal
-  [2, 4, 6]  // Right-to-left diagonal
+    [0, 1, 2], // Верхний ряд
+    [3, 4, 5], // Средний ряд
+    [6, 7, 8], // Нижний ряд
+    [0, 3, 6], // Левый столбик
+    [1, 4, 7], // Средний столбик
+    [2, 5, 8], // Правый столбик
+    [0, 4, 8], // Диагональ слева направо
+    [2, 4, 6]  // Диагональ справа налево
 ];
 
+// Проверка статуса игры
 function checkForWinOrDraw() {
     let roundWon = false;
-
+    // Перебор выигрышных комбинаций
     for (let i = 0; i < winConditions.length; i++) {
         const [a, b, c] = winConditions[i];
         if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
@@ -68,13 +70,13 @@ function checkForWinOrDraw() {
             break;
         }
     }
-
+    // Игрок выиграл
     if (roundWon) {
         announceWinner(currentPlayer);
         gameActive = false;
         return;
     }
-
+    // Ничья
     let roundDraw = !gameBoard.includes('');
     if (roundDraw) {
         announceDraw();
@@ -83,16 +85,32 @@ function checkForWinOrDraw() {
     }
 }
 
+// Объявление победителя
+function announceWinner(player) {
+    message.innerText = `🎉 Player ${player} Wins!`;
+    message.classList.add('message-appear');
+}
+
+// Объявление ничьей
+function announceDraw() {
+    message.innerText = '👍 Game Draw!';
+    message.classList.add('message-appear');
+}
+
+// Добавление кнопки сброса
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', resetGame, false);
+
+// Перезапуск игры
 function resetGame() {
     gameBoard = ['', '', '', '', '', '', '', '', ''];
     gameActive = true;
     currentPlayer = 'X';
     cells.forEach(cell => {
         cell.innerText = '';
+        cell.style.backgroundColor = '#fff';
     });
     message.innerText = '';
     message.classList.remove('message-appear');
-}
 
-const resetButton = document.getElementById('resetButton');
-resetButton.addEventListener('click', resetGame, false);
+}
